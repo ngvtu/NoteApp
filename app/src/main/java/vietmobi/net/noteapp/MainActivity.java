4c40d7c17897cdabc8bdb56847c202a300f2ab27
ngvtu <nvtu1612@gmail.com>
2022-10-25 17:33:32 +0700

    first commit

---
+++ app/src/main/java/vietmobi/net/noteapp/MainActivity.java
@@ -0,0 +1,14 @@
+package vietmobi.net.noteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
